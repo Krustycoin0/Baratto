37f8d8fe5e60883b8bc96b5f795b8777119907de
Krustycoin0 <133280489+Krustycoin0@users.noreply.github.com>
2025-01-04 01:33:55 +0100

    Create Modal.vue

---
+++ components/Modal.vue
@@ -0,0 +1,28 @@
+<template>
+  <dialog ref="modal" class="modal">
+    <div class="modal-box">
+      <div class="modal-header">
+        <h3 class="font-bold text-lg modal-title"><slot name="title"></slot></h3>
+        <button class="btn btn-sm btn-circle" @click="closeModal">✕</button>
+      </div>
+      <slot></slot>
+      <div class="modal-action" v-if="$slots.action">
+        <slot name="action"></slot>
+      </div>
+    </div>
+  </dialog>
+</template>
+
+<script>
+export default {
+  methods: {
+    closeModal() {
+      this.$refs.modal.close();
+      this.$emit('close');
+    },
+    openModal() {
+         this.$refs.modal.showModal();
+    }
+  },
+};
+</script>
